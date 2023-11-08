9c44804e1eeeb25d701a1bd4db35afdc73305fa4
Igor Carvalheira <128088614+Abigor111@users.noreply.github.com>
2023-11-08 19:39:35 +0000

    Added the first functions

--- suntzu/test.py
+++ suntzu/test.py
@@ -1,9 +1,25 @@
-from polars import read_csv
-class Test:
-    def read_file(self, path):
-        if path.endswith(".csv"):
-            df = read_csv(path)
-            df = df.to_pandas()
-            return df
-    def show(self):
-        self.head()
+import polars as pl
+class TestDataFrame(pl.DataFrame):
+    @classmethod
+    def read_file(cls, path):
+        try:
+            df = pl.read_csv(path).to_pandas()
+            return cls(df)
+        except Exception as e:
+            print(f"Erro ao ler o arquivo CSV: {e}")
+            return cls(None)
+
+    def view(self):
+        if self.is_empty():
+            print("Nenhum DataFrame carregado ainda.")
+        else:
+            return self.head()
+
+    def get_dims(self):
+        if self.is_empty():
+            print("Nenhum DataFrame carregado ainda.")
+        else:
+            print(f"Cols: {self.shape[1]}")
+            print(f"Rows: {self.shape[0]}")
+
+

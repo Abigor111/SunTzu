0def582b74c570bba3a5b0eab58440b2105ea559
Igor Carvalheira <128088614+Abigor111@users.noreply.github.com>
2024-05-31 18:41:50 +0100

    Teste

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath(".."))
 extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc"]
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['Thumbs.db', '.DS_Store'] # '_build',
 
 
 
@@ -26,4 +26,4 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_static_path = ['_build']
